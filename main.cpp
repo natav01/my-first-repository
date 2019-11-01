a8bf6076ad396c114533c6c023cba0041fe551c6
nata01 <natalya2001girnyk@gmail.com>
2019-11-01 17:21:08 +0200

    const

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 using namespace std;
 
 int main () {
+const string myPassword{"its123"};
+
 string Friend;
 cout<<"Hello from natav01"<<endl;
 bool PllugisCool ;
@@ -13,7 +15,11 @@ cout<<"Yes it is!!!"<<endl;}
 else { cout<<"No, it is not!!!"<<endl;}
 
 vector  <string> YourFriend;
-
+cout<<"If you know password you can intupe name of your friends!"<<endl;
+cout<<"Please inpute password:"<<endl;
+string password;
+cin>>password;
+if (password == myPassword) {
 for (int i=1; i<4; i++) {
 
 cout<<"Inpute name of your friends"<<endl;
@@ -23,5 +29,6 @@ YourFriend.push_back(Friend);
 
 cout<<"It is your friends:"<<endl;
 for (int i =0; i<YourFriend.size(); i++)
-cout<<YourFriend[i]<<endl;
+cout<<YourFriend[i]<<endl;}
+else cout<<"ERROR! YOU DON'T KNOW A PASSWORD"<<endl;
 }
